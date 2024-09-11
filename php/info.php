9d02a84beaab1977ebecb7f8a8a8770a683809a7
Adedoyin <160499103+anime-kun32@users.noreply.github.com>
2024-09-11 14:09:36 +0100

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,9 +1,9 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
-$apiLink = ""; // add the api here with the '/' at the end!
-$consumet = ""; // host ur own consumet api from here `https://github.com/consumet/api.consumet.org`
+$apiLink = "https://g-ruby-seven.vercel.app/"; // add the api here with the '/' at the end!
+$consumet = "https://no-drab.vercel.app"; // host ur own consumet api from here `https://github.com/consumet/api.consumet.org`
 if (date("d") > 15){
-    $apiLink = ""; // same goes for here
+    $apiLink = "https://g-ruby-seven.vercel.app/"; // same goes for here
 }
 ?>
